c2f080a03c1bcea4d3fe02516e57fd846b34b2fc
Casy <el.casy@gmail.com>
2015-10-26 16:16:11 -0300

    Todas las vistas listas

--- ARK/urls.py
+++ ARK/urls.py
@@ -7,5 +7,22 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^inicio/', 'arkapp.views.inicio'),
+  	url(r'^$', 'arkapp.views.inicio'),
+  	url(r'^inicio/', 'arkapp.views.inicio_busqueda'), 
+  	url(r'^busqueda-avanzada', 'arkapp.views.busqueda_avanzada'), 	
+  	url(r'^resultados-busqueda', 'arkapp.views.resultados_busqueda'), 	  	
+    url(r'^publicar-documento/', 'arkapp.views.publicar_documento'),
+    url(r'^escribir-resena/', 'arkapp.views.publicar_documento'),
+    url(r'^mis-referencias/', 'arkapp.views.mis_referencias'),
+    url(r'^faq/', 'arkapp.views.faq'),
+    url(r'^terminos-y-politicas/', 'arkapp.views.terminos_y_politicas'),
+    url(r'^mis-valoraciones/', 'arkapp.views.mis_valoraciones'),
+    url(r'^mis-publicaciones/', 'arkapp.views.mis_publicaciones'),
+    url(r'^mis-referencias/', 'arkapp.views.mis_referencias'),
+    url(r'^mi-perfil/', 'arkapp.views.mi_perfil'),
+    url(r'^mis-estadisticas/', 'arkapp.views.mis_estadisticas'),
+    url(r'^cerrar-sesion/', 'arkapp.views.cerrar_sesion'),
+    url(r'^consola-usuarios/', 'arkapp.views.consola_usuarios'),
+    url(r'^modificar-datos-usuario/', 'arkapp.views.modificar_datos_usuario'),
+    url(r'^ver-publicacion/', 'arkapp.views.ver_publicacion'),
 ]
--- arkapp/views.py
+++ arkapp/views.py
@@ -12,8 +12,57 @@ from django.contrib.sessions.models import Session
 #from forms import SignUpForm
 
 def inicio(request):
-	return render_to_response('ingresar.html', context_instance=RequestContext(request))
+    return render_to_response('inicio.html', context_instance=RequestContext(request))
 
+def inicio_busqueda(request):
+    return render_to_response('inicio-busqueda.html', context_instance=RequestContext(request))
+
+def resultados_busqueda(request):
+    return render_to_response('resultados-busqueda.html', context_instance=RequestContext(request))
+
+def busqueda_avanzada(request):
+    return render_to_response('busqueda-avanzada.html', context_instance=RequestContext(request))
+
+def publicar_documento(request):
+    return render_to_response('publicar-documento.html', context_instance=RequestContext(request))
+
+def mis_referencias(request):
+    return render_to_response('mis-referencias.html', context_instance=RequestContext(request))
+
+def faq(request):
+    return render_to_response('faq.html', context_instance=RequestContext(request))
+
+def terminos_y_referencias(request):
+    return render_to_response('terminos-y-referencias.html', context_instance=RequestContext(request))
+
+def mis_valoraciones(request):
+    return render_to_response('mis-valoraciones.html', context_instance=RequestContext(request))
+
+def mis_publicaciones(request):
+    return render_to_response('mis-publicaciones.html', context_instance=RequestContext(request))
+
+def mis_referencias(request):
+    return render_to_response('mis-referencias.html', context_instance=RequestContext(request))
+
+def mi_perfil(request):
+    return render_to_response('mi-perfil.html', context_instance=RequestContext(request))
+
+def modificar_datos_usuario(request):
+    return render_to_response('modificar-datos-usuario.html', context_instance=RequestContext(request))
+
+def mis_estadisticas(request):
+    return render_to_response('mis-estadisticas.html', context_instance=RequestContext(request))
+
+def cerrar_sesion(request):
+    return render_to_response('inicio.html', context_instance=RequestContext(request))
+
+def consola_usuarios(request):
+    return render_to_response('consola-usuarios.html', context_instance=RequestContext(request))
+
+
+
+
+'''
 def nuevousuario(request):
     if not request.user.is_anonymous():
         return HttpResponseRedirect('/privado')
@@ -42,4 +91,4 @@ def nuevousuario(request):
         'form': form,
     }
     return render_to_response('nuevousuario.html', data, context_instance=RequestContext(request))
-	
+'''
